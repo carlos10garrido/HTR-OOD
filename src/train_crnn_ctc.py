998f4077dc857dd56310e2d40b37948511a6d2ab
Carlos Garrido <carlos10garrido@gmail.com>
2024-04-24 16:46:33 +0000

    Cleanned comments and train/test only option added

--- src/train_crnn_ctc.py
+++ src/train_crnn_ctc.py
@@ -98,20 +98,6 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, float], Dict[str, Any]]:
     )
 
     # Load a checkpoint if provided from callbacks.model_checkpoint filename
-    # ckpt_path = cfg.callbacks.model_checkpoint.filename if cfg.callbacks.model_checkpoint.filename else None
-
-    # Load a checkpoint if provided from callbacks.model_checkpoint filename
-    # ckpt_path = cfg.callbacks.model_checkpoint.dirpath + cfg.callbacks.model_checkpoint.filename + '.ckpt' if cfg.callbacks.model_checkpoint.filename else None
-    # # if ckpt_path exists, load the model from the checkpoint
-    # if ckpt_path is not None and os.path.exists(ckpt_path):
-    #     print(f'CHECKPOINT PATH EXISTS: {ckpt_path}')
-    #     print(f'MODEL WILL BE LOADED FROM CHECKPOINT: {model}')
-    # else:
-    #     print(f'CHECKPOINT PATH DOES NOT EXIST: {ckpt_path}')
-    #     print(f'MODEL WILL BE TRAINED FROM SCRATCH: {model}')
-    #     ckpt_path = None
-
-    # Load a checkpoint if provided from callbacks.model_checkpoint filename
     ckpt_path = cfg.callbacks.model_checkpoint_base.dirpath + cfg.callbacks.model_checkpoint_base.filename + '.ckpt' if cfg.callbacks.model_checkpoint.filename else None
     # if ckpt_path exists, load the model from the checkpoint
     if ckpt_path is not None and os.path.exists(ckpt_path):
@@ -122,16 +108,23 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, float], Dict[str, Any]]:
         print(f'MODEL WILL BE TRAINED FROM SCRATCH: {model}')
         ckpt_path = None
       
+    model = CRNN_CTC_Module.load_from_checkpoint(ckpt_path, datasets=cfg.get("data")) if ckpt_path is not None else model
 
-    model = CRNN_CTC_Module.load_from_checkpoint(ckpt_path) if ckpt_path is not None else model
-    # Train the model
-    trainer.fit(model, datamodule.train_dataloader(), datamodule.val_dataloader())
+    if cfg.train is True:
+        print(f'TRAINING MODEL: {model}')
+        trainer.fit(model, datamodule.train_dataloader(), datamodule.val_dataloader())
+        trainer.test(model, datamodule.test_dataloader())
+    else:
+        print(f'MODEL WILL NOT BE TRAINED: {model}. Only testing will be performed.')
+        trainer.test(model, datamodule.test_dataloader())
+    
+    # # Train the model
+    # trainer.fit(model, datamodule.train_dataloader(), datamodule.val_dataloader())
     
 
-    # Predict iterating over images
-    # breakpoint()
-    results = trainer.test(model, datamodule.test_dataloader())
-    print(f'PREDICTIONS: {results}')
+    # Test the model
+    # results = trainer.test(model, datamodule.test_dataloader())
+    # print(f'PREDICTIONS: {results}')
 
 
 
