d24abdc62ba66f059e1bce8169ea5ceb7ea1b512
Carlos Garrido <carlos10garrido@gmail.com>
2024-09-28 21:54:06 +0000

    WIP with Michael et al.

--- src/models/components/crnn_michael_att.py
+++ src/models/components/crnn_michael_att.py
@@ -9,26 +9,30 @@ import torch.nn.utils.parametrize as P
 
       
 class ContentBasedAttention(nn.Module):
-    def __init__(self, enc_dim=128, dec_dim=128, att_dim=128):
+    def __init__(self, hidden_size=128):
         super().__init__()
         self.softmax = nn.Softmax(dim=1)
         
-        self.W_h = nn.Linear(dec_dim, att_dim, bias=False)
-        self.W_s = nn.Linear(enc_dim, att_dim, bias=False)
-        self.v = nn.Linear(att_dim, 1, bias=False)
-        self.b = nn.Parameter(torch.Tensor(att_dim).normal_())
+        self.W_h = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_s = nn.Linear(hidden_size, hidden_size, bias=True)
+        self.v = nn.Linear(hidden_size, 1, bias=False)
         
     def forward(self, encoder_outputs, decoder_h_t):
+        # query: decoder hidden state [B, dec_dim, 1] (1 is layer)
+        # key: encoder hidden states [B, T, enc_dim]
+        
         # Bahdanau scoring function
         batch_size, sequence_length, enc_dim = encoder_outputs.size()
         
-        # [batch_size, sequence_length, att_dim]
-        energy = torch.tanh(self.W_h(decoder_h_t).unsqueeze(1).repeat(1, sequence_length, 1) +
-                            self.W_s(encoder_outputs) + self.b)
-        energy = self.v(energy).squeeze(-1)
-        att_weights = self.softmax(energy)
+        query = decoder_h_t.permute(1, 0, 2).contiguous() # [B, 1, dec_dim]
+        keys = encoder_outputs
         
-        return att_weights # [B, T] containing the attention weights for that timestep
+        energy = self.v(torch.tanh(self.W_h(query) + self.W_s(keys))) # [B, T, 1]
+        weights = self.softmax(energy.squeeze(-1)).unsqueeze(1) # [B, 1, T]
+        
+        context = torch.bmm(weights, keys) # [B, 1, enc_dim]
+        
+        return context
         
 
 class CRNN_Michael(nn.Module):
@@ -90,14 +94,14 @@ class CRNN_Michael(nn.Module):
       # Linear projection (original paper implemented as a 1x1 convolutional layer
       # Linear layer is faster since it does not require permutation of the tensor
       self.ctc_pred = nn.Linear(self.hidden_size*2, self.vocab_size+1) # +1 for ctc blank token. These are H in the paper
-      # self.combine_c_h = nn.Linear(self.hidden_size*2, self.hidden_size, bias=True)
       self.combine_c_h = nn.Linear(self.vocab_size+1 + self.hidden_size, self.hidden_size, bias=True)
-      # self.act = nn.Tanh()
+      self.act = nn.Tanh()
       # self.act = nn.ReLU()
-      self.act = nn.Identity()
+      # self.act = nn.Identity()
       
-      self.attention = ContentBasedAttention(self.vocab_size+1, input_size*2, att_dim)
-      self.char_embedding = nn.Embedding(self.vocab_size+1, char_embedding_size)
+      self.attention = ContentBasedAttention(self.hidden_size)
+      self.conv1d = nn.Linear(self.hidden_size*2, self.hidden_size) # Conv1d layer for attention
+      self.char_embedding = nn.Embedding(self.vocab_size, char_embedding_size)
 
       self.decoder = nn.LSTM(
         input_size=char_embedding_size + self.hidden_size,
@@ -108,7 +112,7 @@ class CRNN_Michael(nn.Module):
         batch_first=True
       )
 
-      self.output = nn.Linear(hidden_size*2, self.vocab_size)
+      self.output = nn.Linear(hidden_size, self.vocab_size)
 
     def init_x(self, batch_size):
       return torch.ones((batch_size, 1), dtype=torch.long) * self.tokenizer.bos_id
@@ -117,11 +121,8 @@ class CRNN_Michael(nn.Module):
       # return torch.Tensor(batch_size, num_layers, decoder_dim).normal_()
       return torch.Tensor(batch_size, num_layers, decoder_dim).zero_()
     
-    
-
     # Training forward
     def forward(self, x: torch.Tensor, y: torch.tensor) -> torch.Tensor:
-    # def forward(self, x: torch.Tensor) -> torch.Tensor:
       # breakpoint()
       # CNN encoder
       x = self.cnn_encoder(x)
@@ -130,60 +131,43 @@ class CRNN_Michael(nn.Module):
       # RNN encoder
       self.rnn_encoder.flatten_parameters()
       x, _ = self.rnn_encoder(x)
-
-      # Return x for CTC loss
-      # encoder_outputs = x.clone()
-      # cnn_output = self.ctc_pred(x)
-      x = self.ctc_pred(x)
+      cnn_output = self.ctc_pred(x)
+      x = self.conv1d(x)
+            
       encoder_outputs = x.clone()
 
       # Decoder 
       batch_size, enc_sequence_length, enc_dim = x.size()
       batch_size, dec_sequence_length = y.size()
 
-      h = self.init_h(batch_size, self.hidden_size, self.layers_decoder).to(x.device).permute(1, 0, 2).contiguous()
-      c = torch.zeros(batch_size, self.layers_decoder, self.hidden_size).to(x.device).permute(1, 0, 2).contiguous()
-      context = c # Initalize first context vector with zeros
+      # Layers_decoder = 1 for this particular model
+      h = self.init_h(1, self.hidden_size, self.layers_decoder).to(x.device).permute(1, 0, 2).contiguous()
+      c = torch.zeros(1, self.layers_decoder, self.hidden_size).to(x.device).permute(1, 0, 2).contiguous()
       logit_list = []
 
-      logit_list.append(torch.ones(batch_size, self.vocab_size).to(x.device) * self.tokenizer.bos_id)
+      # logit_list.append(torch.ones(batch_size, self.vocab_size).to(x.device) * self.tokenizer.bos_id)
       
       # Flatten parameters
       self.decoder.flatten_parameters()
       
-      # Add <BOS> token to the beginning of the sequence
-      y = torch.cat([torch.ones(batch_size, 1).to(x.device) * self.tokenizer.bos_id, y], dim=1).long().to(x.device)
-      
       char_embeddings = self.char_embedding(y)
       
-      # h = [1, batch_size, hidden_size]
-      # c = [1, batch_size, hidden_size]
-      # context = [1, batch_size, hidden_size]
-      
       for i in range(dec_sequence_length):
-          char_embed = char_embeddings[:, i, :].unsqueeze(0)
+          char_embed = char_embeddings[:, i].unsqueeze(1)
+          # print(f'CHAR EMBED (training): {char_embed.shape}')
+
+          # Attention 
+          context = self.attention(encoder_outputs, h) # Since we have to squeeze the 1 dimension
+          
           input_embed = torch.cat([char_embed, context], dim=-1).permute(1, 0, 2)
           out, (h, c) = self.decoder(input_embed, (h, c))
           
-          # Attention 
-          alpha = self.attention(encoder_outputs, h[0]) # Since we have to squeeze the 1 dimension
-
-          # Weighted-sum
-          # [batch_size, out_dim]
-          context = torch.sum(alpha.unsqueeze(-1) * encoder_outputs, dim=1).unsqueeze(0)
-          context = torch.cat([context, h], dim=-1) # Generate context vector
-          
-          context = self.act(self.combine_c_h(context)) 
-          
           # [batch_size, vocab_size]
-          logit = self.output(torch.cat([context, h], dim=-1)) # Predict the next character
-
-          # print(f'Logit shape: {logit.shape}')
-          logit = logit.squeeze(0)
+          logit = self.output(out).squeeze(0) # Predict the next character
           logit_list.append(logit)
 
-      # return cnn_output.log_softmax(-1).permute(1, 0, 2), torch.stack(logit_list, dim=1)
-      return torch.stack(logit_list, dim=1) # Only for seq2seq training
+      return cnn_output.log_softmax(-1).permute(1, 0, 2), torch.stack(logit_list, dim=1)
+      # return torch.stack(logit_list, dim=1) # Only for seq2seq training
 
 
     # Inference forward
@@ -195,49 +179,37 @@ class CRNN_Michael(nn.Module):
       # RNN encoder
       self.rnn_encoder.flatten_parameters()
       x, _ = self.rnn_encoder(x)
-
-      # Return x for CTC loss
-      # encoder_outputs = x.clone()
-      # cnn_output = self.ctc_pred(x)
-      x = self.ctc_pred(x)
+      cnn_output = self.ctc_pred(x)
+      x = self.conv1d(x)
+            
       encoder_outputs = x.clone()
       
       # Decoder
       batch_size, enc_sequence_length, enc_dim = x.size()
 
       output = self.init_x(batch_size).to(x.device)
-      h = self.init_h(batch_size, self.hidden_size, self.layers_decoder).to(x.device).permute(1, 0, 2).contiguous()
-      c = torch.zeros(batch_size, self.layers_decoder, self.hidden_size).to(x.device).permute(1, 0, 2).contiguous()
-      context = c # Initalize first context vector with zeros
-      output_list = []
-      output_list.append(torch.ones(batch_size).to(x.device) * self.tokenizer.bos_id)
-      
-      raw_preds = []
+      h = self.init_h(1, self.hidden_size, self.layers_decoder).to(x.device).permute(1, 0, 2).contiguous()
+      c = torch.zeros(1, self.layers_decoder, self.hidden_size).to(x.device).permute(1, 0, 2).contiguous()
+      output_list, raw_preds = [], []
       
       # Flatten parameters
       self.decoder.flatten_parameters()
       
       # output = [batch_size, 1]
-      
+      # breakpoint()
       for i in range(120):
-          char_embed = self.char_embedding(output).permute(1, 0, 2)
+          char_embed = self.char_embedding(output)
+          # print(f'CHAR EMBED (inference): {char_embed.shape}')
+
+          # Attention 
+          context = self.attention(encoder_outputs, h) # Since we have to squeeze the 1 dimension
+          
           input_embed = torch.cat([char_embed, context], dim=-1).permute(1, 0, 2)
           out, (h, c) = self.decoder(input_embed, (h, c))
-
-          # Attention
-          alpha = self.attention(encoder_outputs, h[0]) # Since we have to squeeze the 1 dimension
-
-          # Weighted-sum
-          # [batch_size, out_dim]
-          context = torch.sum(alpha.unsqueeze(-1) * encoder_outputs, dim=1).unsqueeze(0)
-          context = torch.cat([context, h], dim=-1) # Generate context vector
-          
-          context = self.act(self.combine_c_h(context)) 
-          
           
           # [batch_size, vocab_size]
-          logit = self.output(torch.cat([context, h], dim=-1)).squeeze(0) # Predict the next character
-          raw_preds.append(logit.squeeze(0))
+          logit = self.output(out).squeeze(0) # Predict the next character
+          raw_preds.append(logit)
 
           # Greedy Decoding
           logit = torch.argmax(logit, dim=-1)
@@ -255,7 +227,4 @@ if __name__ == "__main__":
 
 
     
-
-
-      
 
